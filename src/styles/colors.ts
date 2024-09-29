2e6363ab21dc53e60b83385d2159b9810a85dca5
Leandro Kimura <kimuradev@gmail.com>
2024-09-29 09:12:31 -0300

    update permissions

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,10 +1,36 @@
 export const colors = {
-    black: "#000",
-    white: "#FFF",
-
-    gray: {
-        300: "D4D4D8",
-        400: "A1A1AA",
-        800: "27272A",
-    }
-}
+    BLACK: '#3b3b3b',
+    BLUE: {
+      LIGHT: '#8AD4FE',
+      MEDIUM: '#55BFFB',
+      DARK: '#24AAF5',
+    },
+    ICE: '#EDEFFC',
+    GRAY: {
+      LIGHT: '#F9F9FF',
+      MEDIUM: '#F2F4FF',
+      DARK: '#EDEFFC',
+    },
+    GREEN: {
+      LIGHT: '#9EE27D',
+      MEDIUM: '#86D262',
+      DARK: '#71C14B',
+    },
+    ORANGE: '#e48e66',
+    PURPLE: {
+      LIGHT: '#A484FF',
+      MEDIUM: '#7F51FE',
+      DARK: '#5B24F5',
+    },
+    RED: {
+      LIGHT: '#F58C92',
+      MEDIUM: '#EE7178',
+      DARK: '#E9525A',
+    },
+    YELLOW: {
+      LIGHT: '#FCDD89',
+      MEDIUM: '#FED569',
+      DARK: '#F5C84F',
+    },
+    WHITE: '#FFF',
+  };
