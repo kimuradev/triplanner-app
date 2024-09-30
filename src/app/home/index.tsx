7948710fcdeb838366f9d4ebcfce2aa20a7f4246
leandro2ki <leandro2ki@gmail.com>
2024-09-29 23:23:32 -0300

    add image to home

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { View, Text } from 'react-native';
+import { View, Image, TextInput, Button } from 'react-native';
 
 export default function Home() {
   return (
-    <View className="flex-1 bg-yellow100 items-center justify-center">
-      <StatusBar style="auto" />
-      <Text>Home</Text>
+    <View className="flex-1 bg-yellow100 items-center justify-center p-8">
+
+      <View>
+        <Image
+          className='absolute'
+          source={require('@/assets/bg.png')}
+        />
+
+        <Image
+          className='w-[300px]'
+          source={require('@/assets/logo.png')}
+        />
+      </View>
+
+      <View className='w-full h-[180px] border rounded bg-white'>
+        <TextInput>Para onde?</TextInput>
+        <TextInput>Quando?</TextInput>
+        <Button title='Continuar' ></Button>
+      </View>
+
     </View>
   );
 }
